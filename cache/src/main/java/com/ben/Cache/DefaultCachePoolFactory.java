d9f58e4417e35e3e25614ef8b2b878fd74ef2114
Ben'Z <20796698@qq.com>
2015-10-14 21:51:02 +0800

    a

---
+++ cache/src/main/java/com/ben/Cache/DefaultCachePoolFactory.java
@@ -0,0 +1,20 @@
+package com.ben.Cache;
+
+
+public class DefaultCachePoolFactory extends AbstaractCachePoolFactory {
+
+	private static final DefaultCachePoolFactory newInstance = new DefaultCachePoolFactory();
+
+	public static final DefaultCachePoolFactory newInstance() {
+		return newInstance;
+	}
+
+	private DefaultCachePoolFactory() {
+	}
+
+	@Override
+	public Pool<Object> getCache() {
+		return DefaultPool.newInstance();
+	}
+
+}
---
+++ util/src/main/java/com/ben/util/Util.java
@@ -0,0 +1,25 @@
+package com.ben.util;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javassist.CtClass;
+
+public class Util {
+	public final static ExecutorService executorService = Executors.newCachedThreadPool();
+
+	public static final ArrayList<String> getParameterTypes(CtClass... c) {
+		ArrayList<String> a = new ArrayList<String>();
+		for (CtClass b : c)
+			a.add(b.getName());
+		return a;
+	}
+
+	public static final ArrayList<String> getParameterTypes(Class... c) {
+		ArrayList<String> a = new ArrayList<String>();
+		for (Class b : c)
+			a.add(b.getName());
+		return a;
+	}
+}
